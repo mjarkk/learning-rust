8b739918854ebcca4e4c09b5b4023d3352745c9f
mjarkk <mkopenga@gmail.com>
2019-09-17 22:37:58 +0200

    I'm totally confused

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,6 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
-struct HtmlText {
-    text: Vec<char>,
-}
-
-// let bad_result: Result<String> = Err();
-impl HtmlText {
-    fn get_letter(&mut self) -> Option<&char> {
-        match self.text.split_first() {
-            Some(val) => {
-                self.text.drain(0..1);
-                return Some(val.0);
-            }
-            None => {
-                return None;
-            }
-        };
-    }
-}
-
-// struct HtmlElArg {
-//     key: String,
-//     value: String,
-// }
-
 struct HtmlEl {
     tag_name: String,
     // args: Vec<HtmlElArg>,
@@ -34,17 +10,25 @@ struct HtmlEl {
 }
 
 fn main() {
+    println!("{}", format_html_text(String::from("cba  abc")));
+
     let html_data = match load_file() {
         Ok(out) => out,
         Err(e) => {
-            println!("{}", e);
+            println!("Recived an error: {}", e);
             return;
         }
     };
 
-    let out = parse_element(&html_data.chars().collect(), true);
+    let out = parse_element(&mut html_data.chars().collect(), true);
+    pritty_print(out, 0);
+}
 
-    println!("Tag: {}, Contents: {}", out.tag_name, out.text_contents)
+fn pritty_print(el: HtmlEl, tabs: usize) {
+    println!("{}{}", "  ".repeat(tabs), el.tag_name);
+    for child in el.childeren {
+        pritty_print(child, tabs + 1);
+    }
 }
 
 enum ParsingStage {
@@ -68,12 +52,21 @@ fn new_text_el(text: String) -> HtmlEl {
     text_el
 }
 
-fn parse_element(chars: &Vec<char>, is_init: bool) -> HtmlEl {
+fn format_html_text(s: String) -> String {
+    let new_lines_replace: Vec<&str> = s.rsplit("\n").collect();
+    let mut out = new_lines_replace.join(" ");
+
+    let spaces: Vec<&str> = out.rsplit(" ").collect();
+    out = spaces.join(" ");
+
+    if out == " " {
+        out = String::new();
+    }
+    out
+}
+
+fn parse_element(chars: &mut Vec<char>, is_init: bool) -> HtmlEl {
     let mut parsing_el = new_html_el();
-    // let mut parsing_arg = HtmlElArg {
-    //     key: String::new(),
-    //     value: String::new(),
-    // };
     let mut current_text = String::new();
     let mut parsing_state = ParsingStage::TagName;
     if is_init {
@@ -82,15 +75,13 @@ fn parse_element(chars: &Vec<char>, is_init: bool) -> HtmlEl {
 
     loop {
         let current_char = match chars.split_first() {
-            Some(x) => {
-                x.0
-            },
+            Some(x) => *(x.0),
             None => return parsing_el,
         };
         chars.drain(0..1);
 
         match parsing_state {
-            ParsingStage::TagName => match *current_char {
+            ParsingStage::TagName => match current_char {
                 '>' => {
                     parsing_state = ParsingStage::Text;
                 }
@@ -98,29 +89,30 @@ fn parse_element(chars: &Vec<char>, is_init: bool) -> HtmlEl {
                     parsing_state = ParsingStage::TagEnd;
                 }
                 _ => {
-                    parsing_el.tag_name.push(*current_char);
+                    parsing_el.tag_name.push(current_char);
                 }
             },
-            ParsingStage::TagEnd => match *current_char {
+            ParsingStage::TagEnd => match current_char {
                 '>' => {
                     return parsing_el;
                 }
                 _ => {}
             },
-            ParsingStage::Text => match *current_char {
+            ParsingStage::Text => match current_char {
                 '<' => {
-                    parsing_el.childeren.push(new_text_el(current_text));
+                    current_text = format_html_text(current_text);
+                    if current_text.len() > 0 {
+                        println!("Adding: {}", current_text.len());
+                        parsing_el.childeren.push(new_text_el(current_text));
+                    }
                     current_text = String::new();
-                    parse_element(chars, false);
+                    parsing_el.childeren.push(parse_element(chars, false));
                 }
                 _ => {
-                    current_text.push(*current_char);
+                    current_text.push(current_char);
                 }
             },
         };
-
-        // match *current_char {
-        // };
     }
 }
 
