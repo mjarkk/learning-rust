9be67909583cfe06b371c79940d744b20b7e4509
mjarkk <mkopenga@gmail.com>
2019-09-17 19:55:57 +0200

    Pushed progression

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,35 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
-struct HtmlElArg {
-    key: String,
-    value: String,
+struct HtmlText {
+    text: Vec<char>,
 }
+
+// let bad_result: Result<String> = Err();
+impl HtmlText {
+    fn get_letter(&mut self) -> Option<&char> {
+        match self.text.split_first() {
+            Some(val) => {
+                self.text.drain(0..1);
+                return Some(val.0);
+            }
+            None => {
+                return None;
+            }
+        };
+    }
+}
+
+// struct HtmlElArg {
+//     key: String,
+//     value: String,
+// }
 
 struct HtmlEl {
     tag_name: String,
-    args: Vec<HtmlElArg>,
+    // args: Vec<HtmlElArg>,
     childeren: Vec<HtmlEl>,
+    text_contents: String,
 }
 
 fn main() {
@@ -22,58 +42,85 @@ fn main() {
         }
     };
 
-    let out = parse_element(&html_data.chars().collect());
+    let out = parse_element(&html_data.chars().collect(), true);
 
-    println!("{}", out.tag_name)
+    println!("Tag: {}, Contents: {}", out.tag_name, out.text_contents)
 }
 
-fn parse_element(chars: &Vec<char>) -> Vec<HtmlEl> {
-    let output:Vec<HtmlEl> = vec![];
+enum ParsingStage {
+    TagName,
+    TagEnd,
+    Text,
+}
 
-    let mut parsing_el = HtmlEl {
+fn new_html_el() -> HtmlEl {
+    HtmlEl {
         tag_name: String::new(),
-        args: vec![],
+        // args: vec![],
         childeren: vec![],
-    };
-    let mut parsing_tag = false;
-    let mut parsing_tag_title = false;
-    let mut parsing_end = true;
+        text_contents: String::new(),
+    }
+}
+
+fn new_text_el(text: String) -> HtmlEl {
+    let mut text_el = new_html_el();
+    text_el.text_contents = text;
+    text_el
+}
+
+fn parse_element(chars: &Vec<char>, is_init: bool) -> HtmlEl {
+    let mut parsing_el = new_html_el();
+    // let mut parsing_arg = HtmlElArg {
+    //     key: String::new(),
+    //     value: String::new(),
+    // };
+    let mut current_text = String::new();
+    let mut parsing_state = ParsingStage::TagName;
+    if is_init {
+        parsing_state = ParsingStage::Text;
+    }
 
     loop {
         let current_char = match chars.split_first() {
-            Some(x) => x.0,
-            None => return output,
+            Some(x) => {
+                x.0
+            },
+            None => return parsing_el,
+        };
+        chars.drain(0..1);
+
+        match parsing_state {
+            ParsingStage::TagName => match *current_char {
+                '>' => {
+                    parsing_state = ParsingStage::Text;
+                }
+                '/' => {
+                    parsing_state = ParsingStage::TagEnd;
+                }
+                _ => {
+                    parsing_el.tag_name.push(*current_char);
+                }
+            },
+            ParsingStage::TagEnd => match *current_char {
+                '>' => {
+                    return parsing_el;
+                }
+                _ => {}
+            },
+            ParsingStage::Text => match *current_char {
+                '<' => {
+                    parsing_el.childeren.push(new_text_el(current_text));
+                    current_text = String::new();
+                    parse_element(chars, false);
+                }
+                _ => {
+                    current_text.push(*current_char);
+                }
+            },
         };
 
-        match *current_char {
-            '<' => {
-                parsing_tag = true;
-                parsing_tag_title = true;
-            }
-            '>' => {
-                if parsing_tag {
-                    parsing_tag = false;
-                    parsing_tag_title = false;
-                    parsing_end = false;
-                    if !parsing_end {
-                        el.childeren = parse_element(chars);
-                    }
-                }
-            }
-            ' ' => {
-                if parsing_tag && parsing_tag_title {
-                    parsing_tag_title = false;
-                }
-            }
-            '=' => {}
-            '"' => {}
-            '\'' => {}
-            _ => {
-                if parsing_tag && parsing_tag_title {
-                    el.tag_name.push(*current_char);
-                }
-            }
-        };
+        // match *current_char {
+        // };
     }
 }
 
