e0af04d2964f7826b815ad4b1e7a2bfa45af499a
mjarkk <mkopenga@gmail.com>
2019-09-19 21:11:15 +0200

    pushed code

---
+++ html-to-jade/README.md
@@ -0,0 +1,6 @@
+# html-to-[jade](http://jade-lang.com/)
+
+```sh
+# Inside this folder run:
+cargo run
+```
--- html-to-jade/src/main.rs
+++ html-to-jade/src/main.rs
@@ -2,9 +2,14 @@ use std::fs::File;
 use std::io;
 use std::io::Read;
 
+struct HtmlElArg {
+    key: String,
+    value: String,
+}
+
 struct HtmlEl {
     tag_name: String,
-    // args: Vec<HtmlElArg>,
+    args: Vec<HtmlElArg>,
     childeren: Vec<HtmlEl>,
     text_contents: String,
 }
@@ -25,8 +30,15 @@ fn main() {
 fn pritty_print(el: HtmlEl, mut tabs: usize, is_start: bool) {
     if !is_start {
         let mut to_print = "  ".repeat(tabs) + &el.tag_name;
+        if el.args.len() > 0 {
+            to_print = to_print + "(";
+            for arg in el.args {
+                to_print = to_print + &arg.key.to_string();
+            }
+            to_print = to_print + ")";
+        }
         if el.text_contents.len() > 0 {
-            to_print = to_print + "| " + &el.text_contents;
+            to_print = to_print + " " + &el.text_contents;
         }
         println!("{}", to_print);
     }
@@ -41,6 +53,8 @@ fn pritty_print(el: HtmlEl, mut tabs: usize, is_start: bool) {
 
 enum ParsingStage {
     TagName,
+    TagArgName,
+    TagArgValue,
     TagEnd,
     Text,
 }
@@ -48,9 +62,16 @@ enum ParsingStage {
 fn new_html_el() -> HtmlEl {
     HtmlEl {
         tag_name: String::new(),
-        // args: vec![],
+        args: vec![],
         childeren: vec![],
         text_contents: String::new(),
+    }
+}
+
+fn new_html_el_arg() -> HtmlElArg {
+    HtmlElArg {
+        key: String::new(),
+        value: String::new(),
     }
 }
 
@@ -84,6 +105,7 @@ fn format_html_text(s: String) -> String {
 
 fn parse_element(chars: &mut Vec<char>, is_init: bool) -> HtmlEl {
     let mut parsing_el = new_html_el();
+    let mut parsing_arg = new_html_el_arg();
     let mut current_text = String::new();
     let mut parsing_state = ParsingStage::TagName;
     if is_init {
@@ -102,11 +124,62 @@ fn parse_element(chars: &mut Vec<char>, is_init: bool) -> HtmlEl {
                 '>' => {
                     parsing_state = ParsingStage::Text;
                 }
+                ' ' => {
+                    parsing_state = ParsingStage::TagArgName;
+                }
                 '/' => {
                     parsing_state = ParsingStage::TagEnd;
                 }
                 _ => {
                     parsing_el.tag_name.push(current_char);
+                }
+            },
+            ParsingStage::TagArgName => match current_char {
+                ' ' => {
+                    if parsing_arg.key.len() > 0 {
+                        parsing_el.args.push(parsing_arg);
+                    }
+                    parsing_arg = new_html_el_arg();
+                }
+                '>' => {
+                    if parsing_arg.key.len() > 0 {
+                        parsing_el.args.push(parsing_arg);
+                    }
+                    parsing_arg = new_html_el_arg();
+                    parsing_state = ParsingStage::Text;
+                }
+                '=' | '"' | '\'' => {
+                    if parsing_arg.key.len() > 0 {
+                        parsing_arg.value.push(current_char);
+                        parsing_state = ParsingStage::TagArgValue;
+                    } else {
+                        parsing_arg.key.push(current_char);
+                    }
+                }
+                _ => {
+                    parsing_arg.key.push(current_char);
+                }
+            },
+            ParsingStage::TagArgValue => match current_char {
+                '>' | '"' | '\'' => {
+                    if parsing_arg.key.len() > 0 {
+                        let x: &[_] = &['"', '\''];
+                        parsing_arg.value = parsing_arg.value.trim_start_matches(x).to_string();
+                        parsing_el.args.push(parsing_arg);
+                    }
+                    parsing_arg = new_html_el_arg();
+                    match current_char {
+                        '"' => {
+                            parsing_state = ParsingStage::TagArgName;
+                        }
+                        '>' => {
+                            parsing_state = ParsingStage::Text;
+                        }
+                        _ => {}
+                    };
+                }
+                _ => {
+                    parsing_arg.value.push(current_char);
                 }
             },
             ParsingStage::TagEnd => match current_char {
